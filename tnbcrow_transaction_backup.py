29a02d0078862ffa39b7271ce20e6857c7b046b1
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-07-21 12:12:13 +0545

    updated crow backup script and created backup:

--- tnbcrow_transaction_backup.py
+++ tnbcrow_transaction_backup.py
@@ -7,6 +7,7 @@ now = datetime.now()
 date_time = now.strftime('%Y_%m_%d_%H_%M_%S')
 
 f = open(f"tnbcrow_transaction_{date_time}.json", "w")
+backup_results = []
 
 next_url = "https://tnbcrow.pythonanywhere.com/recent-trades"
 
@@ -19,9 +20,9 @@ while next_url:
     results = data["results"]
     
     for result in results:
-        result = str(result) + ","
-        f.write(result)
+        backup_results.append(result)
         
     next_url = data["next"]
 
+f.write(json.dumps(backup_results))
 f.close()
